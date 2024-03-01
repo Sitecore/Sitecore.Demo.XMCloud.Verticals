4f22863066e47dc20bf4e1c4e7cbbbc007a27c8b
Alexander Doroshenko <adoprog@users.noreply.github.com>
2024-03-01 12:01:42 -0500

    Added utilities

---
+++ src/platform/Utilities/InitializeHostname.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace XmCloudSXAStarter.Utilities
+{
+    public partial class InitializeHostname : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            using (new Sitecore.SecurityModel.SecurityDisabler())
+            {
+                var site = Request.QueryString["site"];
+                var hostName = Request.QueryString["hostName"];
+
+                if (string.IsNullOrEmpty(site) || string.IsNullOrEmpty(hostName))
+                {
+                    Response.Write("The 'site' or 'hostName' is not provided.");
+                }
+
+                var item = Sitecore.Context.Database.GetItem(
+                    $"/sitecore/content/Verticals/{site}/Settings/Site Grouping/{site}");
+                using (new Sitecore.Data.Items.EditContext(item))
+                {
+                    item["HostName"] = hostName;
+                }
+
+                Response.Write($"Site: {site}");
+                Response.Write($"HostName: {hostName}");
+                Response.Write("Success!");
+            }
+        }
+    }
+}
