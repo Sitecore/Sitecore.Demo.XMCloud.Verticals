89c5c391b96fa1f542bc555e4f92166390de0ed0
Alexander Doroshenko <adoprog@users.noreply.github.com>
2024-04-15 14:58:05 -0400

    Added Storybook

---
+++ src/sxastarter/src/stories/RichText.stories.tsx
@@ -0,0 +1,22 @@
+import { Meta } from '@storybook/react';
+
+import { Default as RichText } from '../components/RichText';
+
+export default {
+  title: 'Components/RichText',
+  component: RichText,
+} as Meta<typeof RichText>;
+
+export const Default = {
+  args: {
+    fields: {
+      text: {
+        value: 'This is a sample Rich Text component',
+      },
+    },
+    rendering: {
+      componentName: 'Rendering',
+      dataSource: '/AnythingForTheWithDatasourceCheckHocToDisplayTheComponent',
+    },
+  },
+};
