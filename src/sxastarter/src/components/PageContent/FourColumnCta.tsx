6185a9a6338547e2cf712d36b9eb5aa45946b977
Alexander Doroshenko <adoprog@users.noreply.github.com>
2024-04-16 13:51:46 -0400

    Added 4 and 5 column examples

---
+++ src/sxastarter/src/components/PageContent/FourColumnCta.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Field } from '@sitecore-jss/sitecore-jss-nextjs';
+
+interface Fields {
+  Text: Field<string>;
+}
+
+export type RichTextProps = {
+  params: { [key: string]: string };
+  fields: Fields;
+};
+
+export const Default = (props: RichTextProps): JSX.Element => {
+  const id = props.params.RenderingIdentifier;
+
+  return (
+    <div
+      className={`container component three-column ${props.params.styles.trimEnd()}`}
+      id={id ? id : undefined}
+    >
+      <div className="row">
+        <div className="col-sm-6 col-md-3">
+          <img width={'100%'} src="https://placehold.co/600x400" alt="1 of 4"></img>
+        </div>
+        <div className="col-sm-6 col-md-3">
+          <img width={'100%'} src="https://placehold.co/600x400" alt="2 of 4"></img>
+        </div>
+        <div className="col-sm-6 col-md-3">
+          <img width={'100%'} src="https://placehold.co/600x400" alt="3 of 4"></img>
+        </div>
+        <div className="col-sm-6 col-md-3">
+          <img width={'100%'} src="https://placehold.co/600x400" alt="4 of 4"></img>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/sxastarter/src/components/PageContent/ThreeColumnCta.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Field } from '@sitecore-jss/sitecore-jss-nextjs';
+
+interface Fields {
+  Text: Field<string>;
+}
+
+export type RichTextProps = {
+  params: { [key: string]: string };
+  fields: Fields;
+};
+
+export const Default = (props: RichTextProps): JSX.Element => {
+  const id = props.params.RenderingIdentifier;
+
+  return (
+    <div
+      className={`container component three-column ${props.params.styles.trimEnd()}`}
+      id={id ? id : undefined}
+    >
+      <div className="row">
+        <div className="col-sm-12 col-md-4">
+          <img width={'100%'} src="https://placehold.co/600x400" alt="1 of 3"></img>
+        </div>
+        <div className="col-sm-12 col-md-4">
+          <img width={'100%'} src="https://placehold.co/600x400" alt="1 of 3"></img>
+        </div>
+        <div className="col-sm-12 col-md-4">
+          <img width={'100%'} src="https://placehold.co/600x400" alt="1 of 3"></img>
+        </div>
+      </div>
+    </div>
+  );
+};
