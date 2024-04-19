aea76cdbf1d8d19c71d3a6ee3db1386f21334146
Alexander Doroshenko <adoprog@users.noreply.github.com>
2024-04-19 12:27:08 -0400

    Added Carousel component

--- src/sxastarter/src/components/PageContent/Carousel.tsx
+++ src/sxastarter/src/components/PageContent/Carousel.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   ComponentParams,
   ComponentRendering,
   Field,
   ImageField,
   Image,
+  RichTextField,
+  LinkField,
+  Text,
+  Link,
 } from '@sitecore-jss/sitecore-jss-nextjs';
 
 interface Fields {
-  Text: Field<string>;
+  Title: Field<string>;
+  Text: RichTextField;
   Image: ImageField;
+  Link: LinkField;
 }
 
 export type CarouselItemProps = {
@@ -26,19 +32,70 @@ interface CarouselComponentProps {
 
 export const Default = (props: CarouselComponentProps): JSX.Element => {
   const id = props.params.RenderingIdentifier;
+  const [index, setIndex] = useState(0);
+
+  const handleNext = () => {
+    setIndex((prevIndex) => (prevIndex < props.fields.items.length - 1 ? prevIndex + 1 : 0));
+  };
+
+  const handlePrev = () => {
+    setIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : props.fields.items.length - 1));
+  };
 
   return (
-    <div
-      className={`container component carousel slide pb-5 ${props.params.styles.trimEnd()}`}
-      id={id ? id : undefined}
-    >
-      <div className="carousel-inner">
-        {props.fields.items.map((item, i) => (
-          <div key={i} className="carousel-item active">
-            <Image field={item.fields.Image} className="d-block w-100" height={' '}></Image>
-          </div>
-        ))}
+    <section className={`component pb-5 ${props.params.styles.trimEnd()}`} id={id ? id : undefined}>
+      <div className="carousel">
+        <ol className="carousel-indicators">
+          {props.fields.items.map((_item, i) => (
+            <li
+              key={i}
+              aria-label="Slide"
+              className={i == index ? 'active' : ''}
+              onClick={() => setIndex(i)}
+            ></li>
+          ))}
+        </ol>
+        <div className="carousel-inner">
+          {props.fields.items.map((item, i) => (
+            <div key={i} className={'carousel-item ' + (i == index ? 'active' : '')}>
+              <Image
+                field={item.fields.Image}
+                className="object-fit-cover d-block w-100"
+                height={' '}
+              ></Image>
+              <div className="side-content col-lg-4 col-md-6 offset-md-4 offset-lg-5 p-5 p-md-0">
+                <h3>
+                  <Text field={item.fields.Title}></Text>
+                </h3>
+                <p>
+                  <Text field={item.fields.Text}></Text>
+                </p>
+                <Link field={item.fields.Link} className="btn btn-danger"></Link>
+              </div>
+            </div>
+          ))}
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="prev"
+          onClick={handlePrev}
+        >
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="next"
+          onClick={handleNext}
+        >
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
       </div>
-    </div>
+    </section>
   );
 };
