11889c0c37e200b96323b265ed721b63f8a0c7f0
Alexander Doroshenko <adoprog@users.noreply.github.com>
2024-04-29 13:07:47 -0400

    Styling updates

--- src/sxastarter/src/components/PageContent/AppPromo.tsx
+++ src/sxastarter/src/components/PageContent/AppPromo.tsx
@@ -29,15 +29,15 @@ export const Default = (props: AppPromoProps): JSX.Element => {
     >
       <div className="container col-xl-12 pt-5">
         <div className="row align-items-center g-lg-5 pt-5">
-          <div className="col-lg-6 text-center text-lg-start">
+          <div className="col-lg-6 text-center text-lg-start ">
             <h1 className="display-4 fw-bold lh-1 mb-3">
               <Text field={props.fields.Title} />
             </h1>
-            <div className="col-lg-10 fs-4">
+            <div className="col-lg-10 fs-4 ">
               <RichText field={props.fields.Text} />
             </div>
           </div>
-          <div className="col-md-10 mx-0 px-0 col-lg-6">
+          <div className="col-md-10 mx-0 px-0 col-lg-6 image-wrapper">
             <Image field={props.fields.Image} className="d-block mx-lg-auto img-fluid"></Image>
           </div>
         </div>
