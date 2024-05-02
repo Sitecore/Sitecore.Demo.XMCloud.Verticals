c6f3dc0b3439a54c74dfcb9ae191c0494de77c2d
Alexander Doroshenko <adoprog@users.noreply.github.com>
2024-05-02 10:44:03 -0400

    Added simple language switcher

--- src/sxastarter/src/components/Navigation/Eyebrow.tsx
+++ src/sxastarter/src/components/Navigation/Eyebrow.tsx
@@ -1,9 +1,47 @@
-import { Placeholder } from '@sitecore-jss/sitecore-jss-nextjs';
+import { Placeholder, useSitecoreContext } from '@sitecore-jss/sitecore-jss-nextjs';
 import { ComponentProps } from 'lib/component-props';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 export const Default = (props: ComponentProps): JSX.Element => {
   const id = props.params.RenderingIdentifier;
+  const languageList = ['en', 'fr-CA'];
+  const languageLabels = ['English', 'Canadian French'];
+
+  const router = useRouter();
+  const { pathname, asPath, query } = router;
+  const { sitecoreContext } = useSitecoreContext();
+
+  const changeLanguage = (lang: string) => {
+    if (pathname && asPath && query) {
+      router.push(
+        {
+          pathname: pathname,
+          query: query,
+        },
+        asPath,
+        {
+          locale: lang,
+          shallow: false,
+        }
+      );
+    }
+  };
+
+  const languageSelector = languageList && languageLabels.length > 0 && (
+    <select
+      onChange={(e) => changeLanguage(e.currentTarget.value)}
+      className="languagePicker"
+      value={sitecoreContext.language}
+      style={{ backgroundColor: '#333', color: '#FFF' }}
+    >
+      {languageList.map((language, index) => (
+        <option key={index} value={language} label={languageLabels[index]} className="languageItem">
+          {languageLabels[index]}
+        </option>
+      ))}
+    </select>
+  );
 
   return (
     <div
@@ -15,9 +53,7 @@ export const Default = (props: ComponentProps): JSX.Element => {
           <div className="col"></div>
           <div className="col-auto">
             <Placeholder name="eyebrow-right" rendering={props.rendering} />
-            <span style={{ paddingLeft: 40 }}>
-              English <i className="fa fa-angle-up"></i>
-            </span>
+            <span style={{ paddingLeft: 40 }}>{languageSelector}</span>
           </div>
         </div>
       </div>
