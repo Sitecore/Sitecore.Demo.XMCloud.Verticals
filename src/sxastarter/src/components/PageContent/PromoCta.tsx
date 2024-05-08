31832ed2cc65924525ec8883949a2146be010666
Alexander Doroshenko <adoprog@users.noreply.github.com>
2024-05-08 19:58:29 -0400

    Update PromoCta.tsx

--- src/sxastarter/src/components/PageContent/PromoCta.tsx
+++ src/sxastarter/src/components/PageContent/PromoCta.tsx
@@ -6,12 +6,16 @@ import {
   RichTextField,
   Text,
   RichText,
+  Link,
+  LinkField,
+  useSitecoreContext,
 } from '@sitecore-jss/sitecore-jss-nextjs';
 
 interface Fields {
   Title: Field<string>;
   Text: RichTextField;
   Image: ImageField;
+  Link: LinkField;
 }
 
 export type PromoCtaProps = {
@@ -21,6 +25,8 @@ export type PromoCtaProps = {
 
 export const Default = (props: PromoCtaProps): JSX.Element => {
   const id = props.params.RenderingIdentifier;
+  const { sitecoreContext } = useSitecoreContext();
+  const isPageEditing = sitecoreContext.pageEditing;
 
   return (
     <div
@@ -28,13 +34,17 @@ export const Default = (props: PromoCtaProps): JSX.Element => {
       id={id ? id : undefined}
     >
       <div className="container col-xl-12 py-5">
-        <div className="row content align-items-center g-lg-5">
-          <div className="col-lg-6 text-center text-lg-start ">
+        <div className="row main-content align-items-center g-lg-5">
+          <div className="col-lg-6 text-center text-lg-start px-5">
             <h1 className="display-4 fw-bold lh-1 mb-3">
               <Text field={props.fields.Title} />
             </h1>
             <div className="col-lg-10 fs-4 ">
               <RichText field={props.fields.Text} />
+
+              {!isPageEditing && props.fields?.Link?.value?.href && (
+                <Link field={props.fields.Link} className="button button-main mt-3" />
+              )}
             </div>
           </div>
           <div className="col-md-10 mx-0 px-0 col-lg-6 image-wrapper">
