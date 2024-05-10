a6960d533b49d18809594eb4472026e3074dd6b4
Neli Kostadinova <nko@sitecore.net>
2024-05-10 11:22:31 +0300

    Four column promo

--- src/sxastarter/src/components/PageContent/FourColumnCta.tsx
+++ src/sxastarter/src/components/PageContent/FourColumnCta.tsx
@@ -40,7 +40,7 @@ export const Default = (props: FourColumnCtaProps): JSX.Element => {
 
   return (
     <div
-      className={`component three-column-cta pb-5 ${props.params.styles.trimEnd()}`}
+      className={`component four-column-cta ${props.params.styles.trimEnd()}`}
       id={id ? id : undefined}
     >
       <div className="container">
@@ -48,57 +48,65 @@ export const Default = (props: FourColumnCtaProps): JSX.Element => {
           <div className="col-sm-12 col-lg-3">
             <div className="content-wrapper">
               <Image field={props.fields.Image1} height={' '} />
-              <h2>
-                <Text field={props.fields.Title1} />
-              </h2>
-              <p>
-                <Text field={props.fields.Text1} />
-              </p>
-              {!isPageEditing && props.fields?.Link1?.value?.href && (
-                <Link field={props.fields.Link1} className="button button-main" />
-              )}
+              <div className="text-wrapper">
+                <h2>
+                  <Text field={props.fields.Title1} />
+                </h2>
+                <p>
+                  <Text field={props.fields.Text1} />
+                </p>
+                {!isPageEditing && props.fields?.Link1?.value?.href && (
+                  <Link field={props.fields.Link1} className="button button-main" />
+                )}
+              </div>
             </div>
           </div>
           <div className="col-sm-12 col-lg-3">
             <div className="content-wrapper">
               <Image field={props.fields.Image2} height={' '} />
-              <h2>
-                <Text field={props.fields.Title2} />
-              </h2>
-              <p>
-                <Text field={props.fields.Text2} />
-              </p>
-              {!isPageEditing && props.fields?.Link2?.value?.href && (
-                <Link field={props.fields.Link2} className="button button-main" />
-              )}
+              <div className="text-wrapper">
+                <h2>
+                  <Text field={props.fields.Title2} />
+                </h2>
+                <p>
+                  <Text field={props.fields.Text2} />
+                </p>
+                {!isPageEditing && props.fields?.Link2?.value?.href && (
+                  <Link field={props.fields.Link2} className="button button-main" />
+                )}
+              </div>
             </div>
           </div>
           <div className="col-sm-12 col-lg-3">
             <div className="content-wrapper">
               <Image field={props.fields.Image3} height={' '} />
-              <h2>
-                <Text field={props.fields.Title3} />
-              </h2>
-              <p>
-                <Text field={props.fields.Text3} />
-              </p>
-              {!isPageEditing && props.fields?.Link3?.value?.href && (
-                <Link field={props.fields.Link3} className="button button-main" />
-              )}
+              <div className="text-wrapper">
+                <h2>
+                  <Text field={props.fields.Title3} />
+                </h2>
+                <p>
+                  <Text field={props.fields.Text3} />
+                </p>
+                {!isPageEditing && props.fields?.Link3?.value?.href && (
+                  <Link field={props.fields.Link3} className="button button-main" />
+                )}
+              </div>
             </div>
           </div>
           <div className="col-sm-12 col-lg-3">
             <div className="content-wrapper">
               <Image field={props.fields.Image4} height={' '} />
-              <h2>
-                <Text field={props.fields.Title4} />
-              </h2>
-              <p>
-                <Text field={props.fields.Text4} />
-              </p>
-              {!isPageEditing && props.fields?.Link4?.value?.href && (
-                <Link field={props.fields.Link4} className="button button-main" />
-              )}
+              <div className="text-wrapper">
+                <h2>
+                  <Text field={props.fields.Title4} />
+                </h2>
+                <p>
+                  <Text field={props.fields.Text4} />
+                </p>
+                {!isPageEditing && props.fields?.Link4?.value?.href && (
+                  <Link field={props.fields.Link4} className="button button-main" />
+                )}
+              </div>
             </div>
           </div>
         </div>
