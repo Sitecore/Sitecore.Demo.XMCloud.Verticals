e39f09074fceffdf5ea3abb7184e07bc622790d9
Christos Papoutsakis <chpa@sitecore.net>
2024-05-17 15:49:00 +0300

    Add Footer component

---
+++ src/sxastarter/src/components/Navigation/Footer.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import {
+  Field,
+  ImageField,
+  Image,
+  LinkField,
+  Link,
+  Text,
+  RichTextField,
+  RichText,
+} from '@sitecore-jss/sitecore-jss-nextjs';
+
+interface Fields {
+  Image1: ImageField;
+  Image2: ImageField;
+  Title1: Field<string>;
+  Text1: RichTextField;
+  Title2: Field<string>;
+  Text2: RichTextField;
+  Title3: Field<string>;
+  Text3: RichTextField;
+  Title4: Field<string>;
+  Text4: RichTextField;
+  Copyright: Field<string>;
+  Link1: LinkField;
+  Link2: LinkField;
+}
+
+export type RichTextProps = {
+  params: { [key: string]: string };
+  fields: Fields;
+};
+
+export const Default = (props: RichTextProps): JSX.Element => {
+  const id = props.params.RenderingIdentifier;
+
+  return (
+    <div
+      className={`component component-spaced footer ${props.params.styles.trimEnd()}`}
+      id={id ? id : undefined}
+    >
+      <div className="container">
+        <div className="content">
+          <div className="logo">
+            <Image field={props.fields?.Image1} className="p-svg" />
+            <Image field={props.fields?.Image2} className="dot-svg" />
+          </div>
+          <div className="row row-cols-1 ps-3 row-cols-sm-2 ps-sm-0 row-cols-xl-4 row-gap-5 gx-5">
+            <div className="col">
+              <div className="title">
+                <Text field={props.fields?.Title1} />
+              </div>
+              <div className="links">
+                <RichText field={props.fields?.Text1} />
+              </div>
+            </div>
+            <div className="col">
+              <div className="title">
+                <Text field={props.fields?.Title2} />
+              </div>
+              <div className="links">
+                <RichText field={props.fields?.Text2} />
+              </div>
+            </div>
+            <div className="col">
+              <div className="title">
+                <Text field={props.fields?.Title3} />
+              </div>
+              <div className="links">
+                <RichText field={props.fields?.Text3} />
+              </div>
+            </div>
+            <div className="col">
+              <div className="title">
+                <Text field={props.fields?.Title4} />
+              </div>
+              <div className="links">
+                <RichText field={props.fields?.Text4} />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="footnote">
+          <Text field={props.fields?.Copyright} />
+          <div className="privacy-links">
+            <Link field={props.fields?.Link1} />
+            <Link field={props.fields?.Link2} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
