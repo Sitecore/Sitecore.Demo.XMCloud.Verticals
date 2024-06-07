584717af4fd9d65151ff3184ef0dec9431a80ca2
Neli Kostadinova <110887014+sc-nelikostadinova@users.noreply.github.com>
2024-06-07 22:25:40 +0300

    AB#9350 Hero Banner #151

---
+++ src/sxastarter/src/components/NonSitecore/DottedAccent.tsx
@@ -0,0 +1,602 @@
+export const DottedAccent = ({ className }: { className?: string }): JSX.Element => {
+  return (
+    <div className={`dotted-accent ${className}`} style={{ opacity: 0.25 }}>
+      <svg xmlns="http://www.w3.org/2000/svg" width="141" height="141" viewBox="0 0 141 141">
+        <g id="Repeat_Grid_1" data-name="Repeat Grid 1" clip-path="url(#clip-path)">
+          <g transform="translate(-5023 287)">
+            <circle
+              id="Ellipse_16"
+              data-name="Ellipse 16"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -287)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_17"
+              data-name="Ellipse 17"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -268)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_18"
+              data-name="Ellipse 18"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -247)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_19"
+              data-name="Ellipse 19"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -229)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_20"
+              data-name="Ellipse 20"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -210)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_21"
+              data-name="Ellipse 21"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -191)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_22"
+              data-name="Ellipse 22"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -172)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_23"
+              data-name="Ellipse 23"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -152)"
+              fill="currentColor"
+            />
+          </g>
+          <g transform="translate(-5004 287)">
+            <circle
+              id="Ellipse_16-2"
+              data-name="Ellipse 16"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -287)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_17-2"
+              data-name="Ellipse 17"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -268)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_18-2"
+              data-name="Ellipse 18"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -247)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_19-2"
+              data-name="Ellipse 19"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -229)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_20-2"
+              data-name="Ellipse 20"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -210)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_21-2"
+              data-name="Ellipse 21"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -191)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_22-2"
+              data-name="Ellipse 22"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -172)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_23-2"
+              data-name="Ellipse 23"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -152)"
+              fill="currentColor"
+            />
+          </g>
+          <g transform="translate(-4985 287)">
+            <circle
+              id="Ellipse_16-3"
+              data-name="Ellipse 16"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -287)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_17-3"
+              data-name="Ellipse 17"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -268)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_18-3"
+              data-name="Ellipse 18"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -247)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_19-3"
+              data-name="Ellipse 19"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -229)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_20-3"
+              data-name="Ellipse 20"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -210)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_21-3"
+              data-name="Ellipse 21"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -191)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_22-3"
+              data-name="Ellipse 22"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -172)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_23-3"
+              data-name="Ellipse 23"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -152)"
+              fill="currentColor"
+            />
+          </g>
+          <g transform="translate(-4966 287)">
+            <circle
+              id="Ellipse_16-4"
+              data-name="Ellipse 16"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -287)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_17-4"
+              data-name="Ellipse 17"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -268)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_18-4"
+              data-name="Ellipse 18"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -247)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_19-4"
+              data-name="Ellipse 19"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -229)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_20-4"
+              data-name="Ellipse 20"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -210)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_21-4"
+              data-name="Ellipse 21"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -191)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_22-4"
+              data-name="Ellipse 22"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -172)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_23-4"
+              data-name="Ellipse 23"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -152)"
+              fill="currentColor"
+            />
+          </g>
+          <g transform="translate(-4947 287)">
+            <circle
+              id="Ellipse_16-5"
+              data-name="Ellipse 16"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -287)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_17-5"
+              data-name="Ellipse 17"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -268)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_18-5"
+              data-name="Ellipse 18"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -247)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_19-5"
+              data-name="Ellipse 19"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -229)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_20-5"
+              data-name="Ellipse 20"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -210)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_21-5"
+              data-name="Ellipse 21"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -191)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_22-5"
+              data-name="Ellipse 22"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -172)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_23-5"
+              data-name="Ellipse 23"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -152)"
+              fill="currentColor"
+            />
+          </g>
+          <g transform="translate(-4928 287)">
+            <circle
+              id="Ellipse_16-6"
+              data-name="Ellipse 16"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -287)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_17-6"
+              data-name="Ellipse 17"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -268)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_18-6"
+              data-name="Ellipse 18"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -247)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_19-6"
+              data-name="Ellipse 19"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -229)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_20-6"
+              data-name="Ellipse 20"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -210)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_21-6"
+              data-name="Ellipse 21"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -191)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_22-6"
+              data-name="Ellipse 22"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -172)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_23-6"
+              data-name="Ellipse 23"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -152)"
+              fill="currentColor"
+            />
+          </g>
+          <g transform="translate(-4909 287)">
+            <circle
+              id="Ellipse_16-7"
+              data-name="Ellipse 16"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -287)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_17-7"
+              data-name="Ellipse 17"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -268)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_18-7"
+              data-name="Ellipse 18"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -247)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_19-7"
+              data-name="Ellipse 19"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -229)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_20-7"
+              data-name="Ellipse 20"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -210)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_21-7"
+              data-name="Ellipse 21"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -191)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_22-7"
+              data-name="Ellipse 22"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -172)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_23-7"
+              data-name="Ellipse 23"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -152)"
+              fill="currentColor"
+            />
+          </g>
+          <g transform="translate(-4890 287)">
+            <circle
+              id="Ellipse_16-8"
+              data-name="Ellipse 16"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -287)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_17-8"
+              data-name="Ellipse 17"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -268)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_18-8"
+              data-name="Ellipse 18"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -247)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_19-8"
+              data-name="Ellipse 19"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -229)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_20-8"
+              data-name="Ellipse 20"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -210)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_21-8"
+              data-name="Ellipse 21"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -191)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_22-8"
+              data-name="Ellipse 22"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -172)"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse_23-8"
+              data-name="Ellipse 23"
+              cx="3"
+              cy="3"
+              r="3"
+              transform="translate(5023 -152)"
+              fill="currentColor"
+            />
+          </g>
+        </g>
+      </svg>
+    </div>
+  );
+};
