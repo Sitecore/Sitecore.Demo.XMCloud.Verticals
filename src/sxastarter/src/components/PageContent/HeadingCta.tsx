5152b87a94baf69746f6292aa39a0f4ff679f3a1
Neli Kostadinova <nko@sitecore.net>
2024-06-12 15:32:56 +0300

    Three Col CTA updates and Heading CTA component

---
+++ src/sxastarter/src/components/PageContent/HeadingCta.tsx
@@ -0,0 +1,126 @@
+import React from 'react';
+import {
+  Field,
+  Text,
+  Link,
+  LinkField,
+  useSitecoreContext,
+} from '@sitecore-jss/sitecore-jss-nextjs';
+
+interface Fields {
+  Eyebrow: Field<string>;
+  Heading: Field<string>;
+  Text: Field<string>;
+  Link: LinkField;
+}
+
+export type HeadingCtaProps = {
+  params: { [key: string]: string };
+  fields: Fields;
+};
+
+export const Default = (props: HeadingCtaProps): JSX.Element => {
+  const id = props.params.RenderingIdentifier;
+  const { sitecoreContext } = useSitecoreContext();
+  const isPageEditing = sitecoreContext.pageEditing;
+
+  return (
+    <div
+      className={`component heading-cta ${props.params.styles.trimEnd()}`}
+      id={id ? id : undefined}
+    >
+      <div className="container">
+        <div className="row gx-5">
+          <div className="col">
+            <div className="heading-content-wrapper">
+              <h6 className="eyebrow-accent">
+                <Text field={props.fields?.Eyebrow} />
+              </h6>
+              <h2 className="display-4 fw-bold">
+                <Text field={props.fields?.Heading} />
+              </h2>
+              <p>
+                <Text field={props.fields?.Text} />
+              </p>
+            </div>
+          </div>
+          <div className="col-12 pt-lg-5 col-lg-auto">
+            {(isPageEditing || props.fields?.Link?.value?.href) && (
+              <Link field={props.fields.Link} className="button button-main" />
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const Compact = (props: HeadingCtaProps): JSX.Element => {
+  const id = props.params.RenderingIdentifier;
+  const { sitecoreContext } = useSitecoreContext();
+  const isPageEditing = sitecoreContext.pageEditing;
+
+  return (
+    <div
+      className={`component heading-cta compact ${props.params.styles.trimEnd()}`}
+      id={id ? id : undefined}
+    >
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className="heading-content-wrapper">
+              <h6 className="eyebrow-accent">
+                <Text field={props.fields?.Eyebrow} />
+              </h6>
+              <h2 className="display-6 fw-bold">
+                <Text field={props.fields?.Heading} />
+              </h2>
+              <p>
+                <Text field={props.fields?.Text} />
+              </p>
+            </div>
+            {(isPageEditing || props.fields?.Link?.value?.href) && (
+              <Link field={props.fields.Link} className="button button-main" />
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const PageHeading = (props: HeadingCtaProps): JSX.Element => {
+  const id = props.params.RenderingIdentifier;
+  const { sitecoreContext } = useSitecoreContext();
+  const isPageEditing = sitecoreContext.pageEditing;
+
+  return (
+    <div
+      className={`component heading-cta ${props.params.styles.trimEnd()}`}
+      id={id ? id : undefined}
+    >
+      <div className="container container-wide">
+        <div className="row gx-5">
+          <div className="col">
+            <div className="heading-content-wrapper">
+              <h6 className="eyebrow-accent">
+                <Text field={props.fields?.Eyebrow} />
+              </h6>
+              <h1 className="display-1 fw-bold">
+                <Text field={props.fields?.Heading} />
+              </h1>
+              <p>
+                <Text field={props.fields?.Text} />
+              </p>
+            </div>
+          </div>
+          <div className="col-12 pt-lg-5 col-lg-auto">
+            {(isPageEditing || props.fields?.Link?.value?.href) && (
+              <Link field={props.fields.Link} className="button button-main" />
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
