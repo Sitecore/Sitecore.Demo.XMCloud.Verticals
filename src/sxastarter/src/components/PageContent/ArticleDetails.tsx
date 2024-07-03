870b43dab4dc9acd70f1184dce60a1bd03f19993
Scott Mulligan <scm@sitecore.net>
2024-07-03 08:53:47 -0400

    Promote develop to main (#204)

--- src/sxastarter/src/components/PageContent/ArticleDetails.tsx
+++ src/sxastarter/src/components/PageContent/ArticleDetails.tsx
@@ -73,3 +73,45 @@ export const Default = (props: PageBackgroundProps): JSX.Element => {
     </div>
   );
 };
+
+export const Simple = (props: PageBackgroundProps): JSX.Element => {
+  const id = props.params?.RenderingIdentifier;
+  return (
+    <div
+      className={`component simple-article-details mt-4 ${props.params?.styles?.trimEnd()}`}
+      id={id ? id : undefined}
+    >
+      <div className="container container-wide">
+        <h1 className="article-title display-1 fw-bold">
+          <Text field={props.fields.Title} />
+        </h1>
+      </div>
+      <div className="container container-widest-fluid">
+        <Image field={props.fields.Thumbnail} className="article-img img-fluid" />
+      </div>
+      <div className="container">
+        <div className="article-content">
+          <div className="row">
+            <div className="col-12 col-lg-6 mx-auto">
+              <p className="article-excerpt fs-5">
+                <Text field={props.fields.Excerpt} />
+              </p>
+              <div className="article-content-body rich-text mt-5">
+                <RichText field={props.fields.Content} />
+              </div>
+              <div className="row article-meta-row">
+                <Placeholder name="article-meta" rendering={props.rendering} />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row mt-5">
+          <Placeholder name="background-page-content" rendering={props.rendering} />
+        </div>
+      </div>
+      <div className="row">
+        <Placeholder name="page-content" rendering={props.rendering} />
+      </div>
+    </div>
+  );
+};
--- src/sxastarter/src/components/PageContent/AuthorWidget.tsx
+++ src/sxastarter/src/components/PageContent/AuthorWidget.tsx
@@ -7,12 +7,20 @@ import {
   withDatasourceCheck,
   ComponentParams,
   ComponentRendering,
+  LinkField,
+  Link,
 } from '@sitecore-jss/sitecore-jss-nextjs';
 
 interface Fields {
   Name: Field<string>;
   Position: Field<string>;
   Photo: ImageField;
+  SocialLink1: LinkField;
+  SocialIcon1: ImageField;
+  SocialLink2: LinkField;
+  SocialIcon2: ImageField;
+  SocialLink3: LinkField;
+  SocialIcon3: ImageField;
 }
 
 export type AuthorWidgetProps = {
@@ -21,7 +29,7 @@ export type AuthorWidgetProps = {
   fields: Fields;
 };
 
-const AuthorWidget = (props: AuthorWidgetProps): JSX.Element => {
+const AuthorWidgetDefault = (props: AuthorWidgetProps): JSX.Element => {
   const id = props.params.RenderingIdentifier;
 
   return (
@@ -46,4 +54,43 @@ const AuthorWidget = (props: AuthorWidgetProps): JSX.Element => {
   );
 };
 
-export default withDatasourceCheck()<AuthorWidgetProps>(AuthorWidget);
+const AuthorWidgetWithSocials = (props: AuthorWidgetProps): JSX.Element => {
+  const id = props.params.RenderingIdentifier;
+
+  return (
+    <div
+      className={`component author-widget with-socials ${props.params.styles.trimEnd()}`}
+      id={id ? id : undefined}
+    >
+      <div className="author-card row g-0">
+        <div className="col-auto">
+          <Image field={props.fields.Photo} className="author-img" />
+        </div>
+        <div className="col">
+          <h6 className="author-name">
+            <Text field={props.fields.Name} />
+          </h6>
+          <p className="author-position">
+            <Text field={props.fields.Position} />
+          </p>
+        </div>
+        <div className="col-12 col-md-auto">
+          <div className="social-links">
+            <Link field={props.fields?.SocialLink1}>
+              <Image field={props.fields?.SocialIcon1} />
+            </Link>
+            <Link field={props.fields?.SocialLink2}>
+              <Image field={props.fields?.SocialIcon2} />
+            </Link>
+            <Link field={props.fields?.SocialLink3}>
+              <Image field={props.fields?.SocialIcon3} />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const Default = withDatasourceCheck()<AuthorWidgetProps>(AuthorWidgetDefault);
+export const WithSocials = withDatasourceCheck()<AuthorWidgetProps>(AuthorWidgetWithSocials);
