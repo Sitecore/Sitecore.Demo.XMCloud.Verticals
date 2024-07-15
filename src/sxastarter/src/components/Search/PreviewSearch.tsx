63744bf092bc3b1f8c41875967f51cc13709ba22
Syed Akhtar <syed.akhtar@sitecore.com>
2024-07-15 13:14:26 +1000

    Added Preview Search

---
+++ src/sxastarter/src/components/Search/PreviewSearch.tsx
@@ -0,0 +1,114 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import type { PreviewSearchInitialState, PreviewSearchWidgetQuery } from '@sitecore-search/react';
+import {
+  FilterAnd,
+  FilterEqual,
+  PageController,
+  WidgetDataType,
+  usePreviewSearch,
+  widget,
+} from '@sitecore-search/react';
+import Link from 'next/link';
+import { ChangeEvent, FormEvent, MouseEvent, useState } from 'react';
+
+type ArticleModel = {
+  id: string;
+  name: string;
+  url: string;
+  source_id?: string;
+};
+
+articles: Array<ArticleModel>;
+
+type InitialState = PreviewSearchInitialState<'itemsPerPage' | 'suggestionsList'>;
+
+interface Props {
+  defaultItemsPerPage: number;
+}
+
+export const PreviewSearch = ({ defaultItemsPerPage }: Props) => {
+  const [search, setSearch] = useState<string>('');
+  const [isSearching, setIsSearching] = useState<boolean>();
+  const sources = process.env.NEXT_PUBLIC_SEARCH_SOURCES;
+
+  PageController.getContext().setLocale({ country: 'au', language: 'en' });
+
+  const {
+    widgetRef,
+    actions: { onItemClick, onKeyphraseChange },
+    queryResult: { isFetching, isLoading, data: { content: previewArticles = [] } = {} } = {},
+  } = usePreviewSearch<ArticleModel, InitialState>({
+    query: (query: PreviewSearchWidgetQuery) => {
+      query
+        .getRequest()
+        .setSearchFilter(new FilterAnd([new FilterEqual('rfk_source.source_id', sources)]));
+    },
+    state: {
+      suggestionsList: [],
+      itemsPerPage: defaultItemsPerPage,
+    },
+  });
+
+  const loading = isLoading || isFetching;
+
+  function keypharaseChangeHandler(e: ChangeEvent<HTMLInputElement>): void {
+    const keyphrase = e.target.value;
+    setIsSearching(true);
+    if (keyphrase.length === 0) setIsSearching(false);
+    setSearch(keyphrase);
+    onKeyphraseChange({ keyphrase: keyphrase });
+  }
+
+  const handleBlur = () => {
+    setTimeout(() => {
+      setIsSearching(false);
+    }, 110);
+  };
+
+  const inputFocus = () => {
+    if (search.length >= 1) {
+      setIsSearching(true);
+    }
+  };
+
+  function handleSearchFormSubmit(e: FormEvent<HTMLFormElement>): void {
+    window.location.href = `/search?q=${search}`;
+  }
+
+  function handleRedirect(article: ArticleModel): void {
+    window.location.href = article.url;
+  }
+
+  return (
+    <>
+      <form
+        className="search-container"
+        onSubmit={handleSearchFormSubmit}
+        onBlur={handleBlur}
+        onFocus={inputFocus}
+      >
+        <input
+          value={search}
+          onChange={keypharaseChangeHandler}
+          className="input-field"
+          placeholder="search here..."
+        />
+
+        {isSearching && previewArticles && previewArticles?.length >= 1 && (
+          <ul className="results-list">
+            {previewArticles.map(
+              (article, index) =>
+                article.name && (
+                  <li key={index} onClick={() => handleRedirect(article)}>
+                    {article.name}
+                  </li>
+                )
+            )}
+          </ul>
+        )}
+      </form>
+    </>
+  );
+};
+const PreviewSearchWidget = widget(PreviewSearch, WidgetDataType.PREVIEW_SEARCH, 'content');
+export default PreviewSearchWidget;
--- src/sxastarter/src/components/Utilities/SearchBox.tsx
+++ src/sxastarter/src/components/Utilities/SearchBox.tsx
@@ -1,8 +1,13 @@
+import { WidgetsProvider } from '@sitecore-search/react';
+import PreviewSearchWidget from 'components/Search/PreviewSearch';
+
 const SearchBox = () => {
+  const customerKey = process.env.NEXT_PUBLIC_SEARCH_CUSTOMER_KEY;
+  const apiKey = process.env.NEXT_PUBLIC_SEARCH_API_KEY;
   return (
-    <>
-      <input className="input-field" placeholder="credit card" />
-    </>
+    <WidgetsProvider env="prod" apiKey={apiKey} customerKey={customerKey} publicSuffix={true}>
+      <PreviewSearchWidget defaultItemsPerPage={6} rfkId="rfkid_6" />
+    </WidgetsProvider>
   );
 };
 
