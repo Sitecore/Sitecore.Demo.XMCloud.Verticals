1898aba7728441fc7b3316996318e008cce9f5d0
Neli Kostadinova <nko@sitecore.net>
2024-09-20 08:09:45 +0300

    Header WithLogoImage variant

--- src/sxastarter/src/components/Navigation/Header.tsx
+++ src/sxastarter/src/components/Navigation/Header.tsx
@@ -1,4 +1,4 @@
-import { Placeholder } from '@sitecore-jss/sitecore-jss-nextjs';
+import { Image, ImageField, Placeholder } from '@sitecore-jss/sitecore-jss-nextjs';
 import { ComponentProps } from 'lib/component-props';
 import React from 'react';
 
@@ -11,6 +11,31 @@ export const Default = (props: ComponentProps): JSX.Element => {
         <div className="row align-items-center">
           <div className="col-auto">
             <Placeholder name="header-left" rendering={props.rendering} />
+          </div>
+          <div className="col">
+            <Placeholder name="header-right" rendering={props.rendering} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export type WithImageProps = ComponentProps & {
+  fields: {
+    LogoImage: ImageField;
+  };
+};
+
+export const WithLogoImage = (props: WithImageProps): JSX.Element => {
+  const id = props.params.RenderingIdentifier;
+
+  return (
+    <div className={`component header ${props.params.styles.trimEnd()}`} id={id ? id : undefined}>
+      <div className={`container container-${props.params?.ContainerWidth?.toLowerCase()}-fluid`}>
+        <div className="row align-items-center">
+          <div className="col-auto">
+            <Image field={props.fields.LogoImage} />
           </div>
           <div className="col">
             <Placeholder name="header-right" rendering={props.rendering} />
