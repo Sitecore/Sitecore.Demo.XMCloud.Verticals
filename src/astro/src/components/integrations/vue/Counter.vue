198c1e438586205a7463500595a9095f275fdb5f
Andrii Dushchak <ad@exdst.com.ua>
2024-11-08 12:06:36 +0200

    added astro project initial commit

---
+++ src/astro/src/components/integrations/vue/Counter.vue
@@ -0,0 +1,32 @@
+<template>
+    <div className="flex">
+      <div>
+        Count: {{ counter }}
+      </div>
+      <button v-on:click="counter += 1" class="btn btn-primary m-2">
+        Increment
+      </button>
+      <button v-on:click="counter -= 1" class="btn btn-primary m-2">
+        Decrement
+      </button>
+    </div>
+</template>
+
+<script>
+
+import { ref } from 'vue'
+export default {
+  name: 'Counter',
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    }
+  },
+  data(props) {
+    return {
+      counter: ref(props.count)
+    }
+  },
+};
+</script>
---
+++ src/astro/src/temp/config.js
@@ -0,0 +1,11 @@
+const config = {};
+config.sitecoreSiteName = import.meta.env.SITECORE_SITE_NAME || 'astro';
+config.sitecoreApiKey = import.meta.env.SITECORE_API_KEY || '{6D3F291E-66A5-4703-887A-D549AF83D859}';
+config.sitecoreApiHost = import.meta.env.SITECORE_API_HOST || 'https://xmcloudcm.localhost';
+config.graphQLEndpointPath = import.meta.env.GRAPH_QL_ENDPOINT_PATH || '/sitecore/api/graph/edge';
+config.graphQLEndpoint = import.meta.env.GRAPH_QL_ENDPOINT || 'https://xmcloudcm.localhost/sitecore/api/graph/edge';
+config.rootItemId = import.meta.env.ROOT_ITEM_ID || '{08D9417D-5071-46FF-896B-EB76B0790526}';
+config.defaultLanguage = import.meta.env.DEFAULT_LANGUAGE || 'en';
+config.fetchWith = import.meta.env.FETCH_WITH || 'GraphQL';
+config.jssAppName = import.meta.env.JSS_APP_NAME || 'Financial';
+export default config;
